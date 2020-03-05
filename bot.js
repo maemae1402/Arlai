68c26103c37413b54021ecbb41fdbbd93ea3d09e
maemae1402 <61832189+maemae1402@users.noreply.github.com>
2020-03-05 20:07:00 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => [
+    console.log('Hello Player, My name is Arlai the D&D bot!')
+        if (message.content === 'ping') [
+            message.reply('pong')
+        ] )])
+
+client.login(process.env.BOT_TOKEN);
